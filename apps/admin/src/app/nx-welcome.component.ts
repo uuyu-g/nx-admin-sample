1d0abd09628608ce56f2be245e3afcb79ce7f2ef
yutaro_elk <36399275+yutaro-elk@users.noreply.github.com>
2022-04-09 12:44:09 +0900

    fix

--- apps/admin/src/app/nx-welcome.component.ts
+++ apps/admin/src/app/nx-welcome.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 /* eslint-disable */
 
 @Component({
-  selector: 'admin-nx-welcome',
+  selector: 'nx-admin-welcome',
   template: `
     <!--
      * * * * * * * * * * * * * * * * * * * * * * * * * * * *
