b4f0c918e913a074e62908fcb86367ed8a9863e5
yutaro_elk <36399275+yutaro-elk@users.noreply.github.com>
2022-04-09 12:16:49 +0900

    rename
    リポジトリ名を変更

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'nx-twitter-alt-root',
+  selector: 'admin-root',
   templateUrl: './app.component.html',
   styles: [],
 })
--- apps/admin/src/app/nx-welcome.component.ts
+++ apps/admin/src/app/nx-welcome.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 /* eslint-disable */
 
 @Component({
-  selector: 'nx-twitter-alt-nx-welcome',
+  selector: 'admin-nx-welcome',
   template: `
     <!--
      * * * * * * * * * * * * * * * * * * * * * * * * * * * *
