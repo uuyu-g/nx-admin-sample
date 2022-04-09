1b5e4e896bd3b7be99a226b428cfc04b2378279a
yutaro_elk <36399275+yutaro-elk@users.noreply.github.com>
2022-04-09 12:09:30 +0900

    add admin app

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-twitter-alt-root',
+  templateUrl: './app.component.html',
+  styles: [],
+})
+export class AppComponent {
+  title = 'admin';
+}
